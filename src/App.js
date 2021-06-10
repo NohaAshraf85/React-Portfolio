ed1bf9bc66f6de5d4f174af3924841a7d0aa24a0
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-06-10 17:52:45 -0400

    cleaning html and creating navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Container from "./components/Container/container"
+import Navbar from "./components/Navbar/navbar"
 
 function App() {
   return (
+    <Navbar />,
     <Container />
   );
 }
