e02722a8743fb99fe80dccffac10a29d0ac79980
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-06-11 11:39:05 -0400

    hero section, missing image

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -3,8 +3,14 @@ import "./hero.css"
 
 function Hero() {
     return(
-        <div className="hero">
-
+        <div className="container hero">
+            <div className="row text-center introText">
+            <div className="col-4-lg col-12-md col-12-sm col-12-xs">
+            <h2>Welcome to my portfolio</h2>
+            <p>My name is Noha, I am an aspiring front-end developer</p>
+              <a className="btn btn-light px-5 viewMyWorkBtn" href="#sampleWork">View My Work</a>
+            </div>
+        </div>
         </div>
     );
 }
