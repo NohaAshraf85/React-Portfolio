7792681adaaf8df220a75f44383e0166b6092bc7
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-06-15 15:59:06 -0400

    fixed navbar links formatting

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -6,10 +6,10 @@ function Navbar() {
     return(
         <div className="navbarclass">
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
                     <Link to="/">
-                    <a className="navbar-brand" href="#">Noha Ashraf</a>
+                    <a className="navbar-brand font-color" href="#">Noha Ashraf</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
