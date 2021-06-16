07d9592d709f69d53d14a70b2bfa49e9c631aa4d
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-06-16 12:54:12 -0400

    made all the buttons open in target_blank

--- src/pages/Portfolio/portfolio.js
+++ src/pages/Portfolio/portfolio.js
@@ -65,42 +65,42 @@ function Portfolio() {
             name: "remoteRecruit",
             description: "A website that connects freelancers and employers",
             github: "https://github.com/lbburnsy/remoteRecruit",
-            deployment: "https://remote-recruit.herokuapp.com/"
+            deployment: "https://remote-recruit.herokuapp.com/",
         },
         {
             img: "/assets/bookHunt1.png",
             name: "Book Hunt",
             description: "Meet your next read and see NYT Best Sellers",
             github: "https://github.com/srad25/Book-Hunt",
-            deployment: "https://srad25.github.io/Book-Hunt/"
+            deployment: "https://srad25.github.io/Book-Hunt/",
         },
         {
             img: "/assets/teamProfileGenerator.png",
             name: "Team Profile Generator",
             description: "A CLI app that creates a webpage display team basic information",
             github: "https://github.com/NohaAshraf85/Team-Profile-Generator",
-            deployment: "https://drive.google.com/file/d/1tDyKklkteBCH80E-3qU6Nj2TzF0YdVxU/view"
+            deployment: "https://drive.google.com/file/d/1tDyKklkteBCH80E-3qU6Nj2TzF0YdVxU/view",
         },
         {
             img: "/assets/weatherDashboardHomepage.png",
             name: "Weather Dashboard",
             description: "creative design for a weather dashboard for the USA States",
             github: "https://github.com/NohaAshraf85/Weather-Dashboard",
-            deployment: "https://nohaashraf85.github.io/Weather-Dashboard/"
+            deployment: "https://nohaashraf85.github.io/Weather-Dashboard/",
         },
         {
             img: "/assets/currentTimeEntry1.png",
             name: "Day Planner",
             description: "Day Planner, 9-5 timeblocks, current, present and future tasks",
             github: "https://github.com/NohaAshraf85/Code-Quiz-Application",
-            deployment: "https://github.com/NohaAshraf85/Day-Planner"
+            deployment: "https://github.com/NohaAshraf85/Day-Planner",
         },
         {
             img: "/assets/codeQuiz.png",
             name: "Code Quiz",
             description: "A Code Quiz Application with timers and scores",
             github: "https://github.com/NohaAshraf85/Code-Quiz-Application",
-            deployment: "https://nohaashraf85.github.io/Code-Quiz-Application/"
+            deployment: "https://nohaashraf85.github.io/Code-Quiz-Application/",
         }
     ]
 
@@ -113,8 +113,8 @@ function Portfolio() {
                 <Card.Text>
                 {card.description}
                 </Card.Text>
-                <Button variant="primary">GitHub</Button>
-                <Button variant="primary">Project</Button>
+                <Button href={card.github} variant="dark" target="_blank">GitHub</Button>
+                <Button href={card.deployment} variant="dark" target="_blank">Project</Button>
             </Card.Body>
             </Card>
 
